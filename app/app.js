2d88dae8c7b277821b6d9ad8c91b2973e15e3e0a
marick2003 <marick2003@users.noreply.github.com>
2018-10-19 18:27:20 +0800

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var a=1;
+//console.log('hello');
+var content=require('./data');
+console.log(content);
+
+var http=require('http');
+http.createServer(function(request,response)
+{
+    
+});
---
+++ app/data.js
@@ -0,0 +1,13 @@
+var data=2;
+// module.exports={
+
+// 		content: data,
+// 		title: "title"
+
+// };
+
+exports.data =data;
+exports.datafun=function(){
+
+	return data;
+}
