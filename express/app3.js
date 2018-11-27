51fa7b4ff2658a51bf2fd5ce3ba83c54cdd76966
marick2003 <marick2003@users.noreply.github.com>
2018-11-27 13:56:29 +0800

    add

--- express/app3.js
+++ express/app3.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var app = express();
-
-var login= function(req,res,next){
-
-    console.log("你是登入狀態2");
+var login=function(req,res,next){
     var _url=req.url;
-    if(_url=='/'){
+    console.log(_url);
+    // if(_url!=='/'){
+    //     next()
+    // }else{
 
-        next()
-    }else{
-
-        console.log("error");
-    }
-   
-    
+    // }
+    next();
 }
+
+///要放第一個排序
+app.get('/',login,function(req,res){
+    res.send('<html><head></head><body><h1>1234index</h1></body></html>')
+})
+app.use(function(req,res,next){
+    console.log('有人進來了');
+    next();
+})
+
 app.use(function(req,res,next){
     res.status(404).send('抱歉，您的頁面找不到')
 })
@@ -23,11 +28,6 @@ app.use(function(err,req,res,next){
     res.status(500).send('程式有些問題，請稍後嘗試')
 })
 
-app.get('/',login,function(req,res){
-
-    res.send('<html><head></head><body><h1>1234index</h1></body></html>')
-
-})
 
 // 監聽 port
 var port = process.env.PORT || 3000;
