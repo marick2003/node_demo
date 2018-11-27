2454333b4ba3732c6c014daa0b71401abb681b43
marick2003 <marick2003@users.noreply.github.com>
2018-11-27 13:04:19 +0800

    add

--- express/app3.js
+++ express/app3.js
@@ -4,9 +4,25 @@ var app = express();
 var login= function(req,res,next){
 
     console.log("你是登入狀態2");
-    next()
+    var _url=req.url;
+    if(_url=='/'){
+
+        next()
+    }else{
+
+        console.log("error");
+    }
+   
     
 }
+app.use(function(req,res,next){
+    res.status(404).send('抱歉，您的頁面找不到')
+})
+
+app.use(function(err,req,res,next){
+    res.status(500).send('程式有些問題，請稍後嘗試')
+})
+
 app.get('/',login,function(req,res){
 
     res.send('<html><head></head><body><h1>1234index</h1></body></html>')
