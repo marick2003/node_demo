6e9035f2e6219c2b948101416cfaa53da735115c
marick2003 <marick2003@users.noreply.github.com>
2018-12-21 19:14:06 +0800

    add

--- express/app7.js
+++ express/app7.js
@@ -8,13 +8,26 @@ app.set('view engine','ejs');
 
 app.use(express.static('public'))
 
-app.use(bodyParser.json)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/',function(req,res){
 
     res.send('now page index');
     console.log('有人造訪首頁');
 
+})
+app.get('/search',function(req,res){
+
+    res.render('search');
+
+})
+
+app.post('/searchList',function(req,res){
+    console.log(req.body);
+    //轉址
+    res.redirect('search');
+   // res.render('search');
 })
 
 var port=process.env.PORT || 3000;
