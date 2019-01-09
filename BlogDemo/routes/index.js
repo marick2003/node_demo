c3a74374f6fbcebd918aceb2c7be34f4258bcb4f
marick2003 <mmaarriicckk@gmail.com>
2019-01-10 03:25:43 +0800

    add

--- BlogDemo/routes/index.js
+++ BlogDemo/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var firebaseAdminDb=require('../connections/firebase_admin');
 
+const ref= firebaseAdminDb.ref('any');
+ref.once('value',function(sanpshot){
+  console.log(sanpshot.val());
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
