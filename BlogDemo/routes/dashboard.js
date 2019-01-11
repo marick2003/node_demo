012c42fe555559f3240f5d86b7d6a0f8178cac92
marick2003 <marick2003@users.noreply.github.com>
2019-01-11 14:14:48 +0800

    add

--- BlogDemo/routes/dashboard.js
+++ BlogDemo/routes/dashboard.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var firebaseAdminDb=require('../connections/firebase_admin');
 
+const categoriesRef=firebaseAdminDb.ref('/categories')
+
  router.get('/article', function(req, res, next) {
     res.render('dashboard/article', { title: 'Express' });
   });
@@ -11,13 +13,31 @@ var firebaseAdminDb=require('../connections/firebase_admin');
   });
   
   router.get('/categories', function(req, res, next) {
-    res.render('dashboard/categories', { title: 'Express' });
+    categoriesRef.once('value').then(function(snapshot){
+     
+      const categories=snapshot.val();
+      console.log(categories);
+      res.render('dashboard/categories', { 
+        title: 'Express',
+        categories
+       });
+    });
+
+
+    
   });
   
 router.post('/categories/create',function(req,res){
 
     const data=req.body;
     console.log(data);
+    const categoryRef = categoriesRef.push();
+    const key= categoryRef.key;
+    data.id=key;
+    categoryRef.set(data).then(function(){
+
+        res.redirect('/dashboard/categories');
+    })
 
 
 });
