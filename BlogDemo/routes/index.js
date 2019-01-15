5be9a3124a6a7c304ba45c2b68bbab48ce0a93b5
marick2003 <mmaarriicckk@gmail.com>
2019-01-16 03:48:19 +0800

    add

--- BlogDemo/routes/index.js
+++ BlogDemo/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var firebaseAdminDb=require('../connections/firebase_admin');
+
+const categoriesRef=firebaseAdminDb.ref('/categories');
+const articlesRef=firebaseAdminDb.ref('/articles');
+
 ////標籤與日期轉換 套件
 const stringtags=require('striptags');
 const momont= require('moment');
@@ -12,7 +16,33 @@ const momont= require('moment');
 // });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ 
+  let categories={};
+  categoriesRef.once('value').then(function(snapshot){
+
+    categories= snapshot.val();
+   // console.log(categories);
+    return articlesRef.orderByChild('update_time').once('value');
+  }).then(function(snapshot){
+   const  articles= [];
+   snapshot.forEach(function(snapshotChild){
+
+      //console.log('child'+snapshotChild.val());
+     
+        articles.push(snapshotChild.val());
+     
+      
+   });
+  // console.log(articles);
+    res.render('index', {
+       title: 'Express',
+       categories,
+       articles,
+       stringtags,
+       momont
+      });
+  });
+  
 });
 
 router.get('/post', function(req, res, next) {
