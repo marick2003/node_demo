e41a933ed94c6dec8c6bd077dfc9d067a55cb107
marick2003 <mmaarriicckk@gmail.com>
2019-01-17 10:04:06 +0800

    add

--- BlogDemo/connections/firebase_connect.js
+++ BlogDemo/connections/firebase_connect.js
@@ -2,12 +2,12 @@
   var firebase=require('firebase');
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBTLe_wrvBO8b_RlQhLqqto6dvykDQDfGE",
-    authDomain: "testbot-993f2.firebaseapp.com",
-    databaseURL: "https://testbot-993f2.firebaseio.com",
-    projectId: "testbot-993f2",
-    storageBucket: "testbot-993f2.appspot.com",
-    messagingSenderId: "837912342372"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASEURL,
+    projectId:  process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId
   };
   firebase.initializeApp(config);
   module.exports=firebase;
--- BlogDemo/routes/auth.js
+++ BlogDemo/routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var firebaseAdminDb=require('../connections/firebase_admin');
+var firebaseClient=require('../connections/firebase_connect');
 router.get('/', function(req, res, next) {
 
     
